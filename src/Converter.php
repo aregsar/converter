e0eac2f56f97f52e16784507b2e23ae622a5e12c
Areg Sarkissian <aregsar@gmail.com>
2021-11-04 15:12:49 -0700

    update

--- src/Converter.php
+++ src/Converter.php
@@ -6,11 +6,18 @@ class Converter
 {
     public function __construct(private ?array $currencyMap = null)
     {
-        $this->currencyMap = $currencyMap ?? ['MRK' => 2];
+        $this->currencyMap = $currencyMap ?? ['EUR' => 2];
     }
 
     public function convert(string $currency): ?int
     {
+        if (!array_key_exists(
+            $currency,
+            $this->currencyMap
+        )) {
+            return null;
+        }
+
         return $this->currencyMap[$currency];
     }
 }
