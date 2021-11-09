adf2f3197683dab727c2016f0c0e32128a7d6b68
Areg Sarkissian <aregsar@gmail.com>
2021-11-09 13:25:53 -0800

    update

---
+++ src/ConverterServiceProvider.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Acme\Converter;
+
+use Illuminate\Support\ServiceProvider;
+
+class ConverterServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     */
+    public function boot()
+    {
+        if ($this->app->runningInConsole()) {
+            //Add resource publishing code here
+            //Add package commands here
+        }
+
+        //add resource loading code here
+    }
+
+    /**
+     * Register the application services.
+     */
+    public function register()
+    {
+        //Add configuration loading code here
+
+        //Add Laravel container service registration code here
+    }
+}
