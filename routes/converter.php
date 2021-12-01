1fe0b94993a5f9632ca4a221c96811f314fa3f77
Areg Sarkissian <aregsar@gmail.com>
2021-12-01 12:55:20 -0800

    update

--- routes/converter.php
+++ routes/converter.php
@@ -2,9 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 
-// Route::middleware(['web'])->group(function () {
-Route::get("convert/{currency}", function ($currency) {
-    $amount = \Converter::convert($currency);
-    return $amount ?? "Currency not supported";
-})->name("convert");
-// });
+Route::middleware(['web'])->group(function () {
+    Route::get("convert/{currency}", function ($currency) {
+        $amount = \Converter::convert($currency);
+        return $amount ?? "Currency not supported";
+    })->name("convert");
+});
