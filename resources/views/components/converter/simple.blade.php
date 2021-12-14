e25702d8fde36c668d65121250ab40665294d8f2
Areg Sarkissian <aregsar@gmail.com>
2021-12-14 14:49:15 -0800

    update

--- resources/views/components/converter/simple.blade.php
+++ resources/views/components/converter/simple.blade.php
@@ -1,3 +1,4 @@
 <div>
     <h3>Simple Component</h3>
+    <h3>{{ $slot }}</h3>
 </div>
--- resources/views/converter/convert.blade.php
+++ resources/views/converter/convert.blade.php
@@ -1,4 +1,6 @@
-@section('content')
-<h1>Converter View Result {{ $amount }}</h1>
-<x-acme-converter::converter.simple />
-@endsection
+<x-acme-converter::layouts.converter>
+    <h1>Converter View Result {{ $amount }}</h1>
+    <x-acme-converter::converter.simple>
+        This content is plugged into the simple.blade.php view file slot
+    </x-acme-converter::converter.simple>
+</x-acme-converter::layouts.converter>
