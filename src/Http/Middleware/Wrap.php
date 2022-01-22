a645072193b51cdf36c1ed620c59fc95ff8c0bfb
Areg Sarkissian <aregsar@gmail.com>
2022-01-22 12:11:11 -0800

    update

---
+++ src/Http/Middleware/Wrap.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Aregsar\Converter\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class Wrap
+{
+    public function handle(Request $request, Closure $next)
+    {
+        //inject header in request
+        $request->headers->set("in", "incoming request");
+
+        //trigger a call to the next middleware in the pipeline and get back the response from the next middlware.
+        $response = $next($request);
+
+        // add code to inject header in the response
+        //$response->header("out", "outgoing response");
+        $response->headers->set("out", "outgoing response");
+
+        return $response;
+    }
+}
---
+++ tests/Http/Middleware/WrapTest.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Aregsar\Converter\Tests\Http\Middleware;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Aregsar\Converter\Http\Middleware\Wrap;
+use Aregsar\Converter\Tests\BaseTestCase;
+
+class WrapTest extends BaseTestCase
+{
+    // protected function tearDown(): void
+    // {
+    //     //Add cleanup code here before the parent tearDown
+    //     \Mockery::close();
+    //     parent::tearDown();
+    // }
+
+    /** @test */
+    function it_adds_in_header_to_request_and_out_header_to_response()
+    {
+        $request = new Request();
+
+        // $response = \Mockery::Mock(Response::class)
+        //     ->shouldReceive('header')
+        //     ->with('Access-Control-Allow-Origin', '*')
+        //     ->once()
+        //     ->getMock();
+
+        $response = \Illuminate\Support\Facades\Response::shouldReceive('header')
+            ->with("out", "outgoing response")
+            ->once()
+            ->getMock();
+
+        //$response = new Response();
+
+        $wrappedResponse = (new Wrap())->handle($request, function ($request)  use ($response) {
+
+            //we want to check that the header was added before this closure is called
+            //by the Wrap middleware handle method, so we need to check assert inside this closure not
+            $this->assertEquals("incoming request", $request->headers->get("in"));
+
+
+            //we pass the mock response to our closure so it can return it to the Wrap::handle method
+            //that the closure is passed to, where the Wrap::handle method will add the out header to mock response
+            //and then return the modified response which we set to $wrappedResponse
+            return $response;
+        });
+
+        $this->assertEquals("outgoing response", $wrappedResponse->headers->get("out"));
+    }
+}
