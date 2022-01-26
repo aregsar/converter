7031c36c0cfd6a630ee51181e71683012e900748
Areg Sarkissian <aregsar@gmail.com>
2022-01-26 11:35:45 -0800

    update

---
+++ tests/Fixtures/Doubles/TestUser.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Aregsar\Converter\Tests\Fixtures\Doubles;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+use Aregsar\Converter\Traits\HasManyNotes;
+use Aregsar\Converter\Tests\Fixtures\Factories\TestUserFactory;
+
+
+class TestUser extends Authenticatable
+{
+    use HasManyNotes, HasFactory;
+
+    protected $guarded = [];
+
+    protected $table = 'acmeusers';
+
+    protected static function newFactory()
+    {
+        return TestUserFactory::new();
+    }
+}
